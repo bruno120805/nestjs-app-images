401fdc70173abf19db41b514b43ad366d2cbaed8
Bruno Garcia <brunogg92678@gmail.com>
2024-11-07 18:25:38 -0600

    adding env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { ImagesModule } from './images/images.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthModule,
     PrismaModule,
     PassportModule.register({
